3b1e1268000246e3f82ce8076b8e24856c58546a
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-10 15:07:29 +0400

    feat: navbar elements added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,21 @@
-import React from "react";
 import "./style.scss";
 function Navbar() {
   return (
-    <div className="Navbar  w-full flex items-center underline">Navbar</div>
+    <div className="Navbar flex items-center justify-center py-4">
+      <div className="container   w-full flex items-center justify-between">
+        <div className="logo">logo</div>
+        <button>catalog</button>
+        <div className="searchbar">
+          <input type="text" />
+        </div>
+        <div className="actions">
+          <button>orders</button>
+          <button>orders</button>
+          <button>orders</button>
+          <button>orders</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
