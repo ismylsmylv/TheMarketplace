edabaf0a179dc52b5f2f0d3c8792c2c921ee5672
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-10 15:02:14 +0400

    fix: tailwind import & sass variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Vite + React</h1>
     </>
   );
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.scss";
+function Navbar() {
+  return (
+    <div className="Navbar  w-full flex items-center underline">Navbar</div>
+  );
+}
+
+export default Navbar;
