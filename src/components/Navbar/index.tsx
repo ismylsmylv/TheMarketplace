14e0aaecf4b2beab87d3a31e6b993b96dad75137
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-10 16:26:44 +0400

    style: icons added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style.scss";
+import {
+  faBoxOpen,
+  faCartShopping,
+  faHeart,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
   return (
     <div className="Navbar flex items-center justify-center py-4">
@@ -9,10 +16,18 @@ function Navbar() {
           <input type="text" />
         </div>
         <div className="actions">
-          <button>orders</button>
-          <button>orders</button>
-          <button>orders</button>
-          <button>orders</button>
+          <button>
+            <FontAwesomeIcon icon={faBoxOpen} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faHeart} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faCartShopping} />
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faUser} />
+          </button>
         </div>
       </div>
     </div>
