2960f018bb77075d8f38b69e3f61cfe6fe7ff7fd
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-12 19:36:55 +0400

    style: search bar updated

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./style.scss";
 type Props = {};
 
 function SearchBar({}: Props) {
   return (
-    <div className="SearchBar">
-      <select name="" id="">
+    <div className="SearchBar rounded-xl w-full max-w-xl flex items-center justify-between">
+      <select name="" id="" className=" p-2   rounded-xl capitalize ">
         <option value="">all categories</option>
         <option value="">all categories</option>
         <option value="">all categories</option>
         <option value="">all categories</option>
       </select>
-      <input type="text" placeholder="Search in the market" />
-      <button>search</button>
+      <div className="divider mx-2 ml-3"></div>
+      <div className="flex items-center justify-start w-full">
+        <input
+          type="text"
+          placeholder="Search in the market"
+          className=" p-3   rounded-xl w-full"
+        />
+        <button className="p-3   ">
+          <FontAwesomeIcon icon={faMagnifyingGlass} color="#fff" />
+        </button>
+      </div>
     </div>
   );
 }
