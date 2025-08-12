358a972d06ea9167e24d3524903513466a9dba42
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-12 22:08:21 +0400

    fix: grid layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,7 +7,7 @@ function Card({}: Props) {
         src="https://laptopmedia.com/wp-content/uploads/2023/12/10-5.jpg"
         alt=""
       />
-      <div className="infos p-3 pb-1">
+      <div className="infos p-3 ">
         <b>2500 AZN</b>
         <p className="name line-clamp-1">Apple MacBook Pro (14-inch, M3)</p>
         <p className="date text-xs mt-2">Baku, today, 13:43</p>
--- src/components/CardGrid/index.tsx
+++ src/components/CardGrid/index.tsx
@@ -1,10 +1,10 @@
 import Card from "../Card";
-
+import "./style.scss";
 type Props = {};
 
 function CardGrid({}: Props) {
   return (
-    <div className="CardGrid ">
+    <div className="CardGrid gap-3 container p-3">
       <Card />
       <Card />
       <Card />
