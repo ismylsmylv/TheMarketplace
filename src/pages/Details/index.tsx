4e2b61c30c8142ab0d0df787a2a0b9ec3929f039
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-16 17:14:17 +0400

    feat: detail infos added

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -7,7 +7,7 @@ import {
 function Details() {
   return (
     <div className="Details container p-4">
-      <div className="topLine flex items-center justify-between">
+      <section className="topLine flex items-center justify-between">
         <strong className="text-2xl">Apple MacBook Pro (14-inch, M3)</strong>
         <div className="actions flex items-center justify-end gap-7 ">
           <button>
@@ -19,7 +19,24 @@ function Details() {
             <p className="text-gray-500 ">Report</p>
           </button>
         </div>
-      </div>
+      </section>
+      <section className="mt-5">
+        <div className="gallery"></div>
+        <div className="info rounded p-5">
+          <div className="price text-3xl font-bold mb-6 pb-4">1500 AZN</div>
+          <div className="flex items-center justify-start">
+            <img
+              src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+              alt="Profile picture"
+            />
+            <div className="infos">
+              <h1 className="font-bold">Sam Altman</h1>
+              <div className="location ">Baku, Yasamal</div>
+              <div className="rate text-xs">90% success rate</div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
