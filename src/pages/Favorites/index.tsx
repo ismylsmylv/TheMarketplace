b8aa6c1f2320942c4ac1a2b456283eef0359535f
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-08-16 16:35:16 +0400

    feat: favorites page updated

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+import "./style.scss";
 function Favorites() {
-  return <div>Favorites</div>;
+  return (
+    <div className="Favorites container flex items-center justify-center flex-col  h-full py-5">
+      <b>Add ads to your favorites list to view them later</b>
+      <Link to={"/"} className="rounded py-2 px-4 text-white mt-5">
+        Browse
+      </Link>
+    </div>
+  );
 }
 
 export default Favorites;
