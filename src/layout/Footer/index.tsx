c99febf4ee7195b51b9dc9303d09a6170cda5656
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-09-01 21:11:19 +0400

    chore: footer component added

---
+++ src/layout/Footer/index.tsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+function Footer() {
+  return (
+    <div className="Footer">
+      <img src="" alt="" />
+      <div className="links"></div>
+      <div className="credits"></div>
+    </div>
+  );
+}
+
+export default Footer;
